63579d14cab0ce1668039fe977cccc00d8c79063
ajayramola24 <ajayramola24@windows-server-2016-1.asia-south1-c.c.amplified-lamp-239212.internal>
2019-05-20 13:42:59 +0000

    c

---
+++ SeleniumDemo/src/alerts/CaptureMessageOfPromptAlert.java
@@ -0,0 +1,41 @@
+package alerts;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class CaptureMessageOfPromptAlert {
+
+	private static String driverPath;
+	private static String URL = "https://javascript.info/alert-prompt-confirm#prompt";
+	private static WebElement element;
+
+	public static void main(String[] args) {
+		try {
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\Public\\chromedriver.exe");
+			WebDriver driver = new ChromeDriver();
+			driver.get(URL);
+
+			WebElement run = driver.findElement(By.xpath("//a[contains(text(),'Run the demo')]"));
+
+			JavascriptExecutor js = (JavascriptExecutor) driver;
+			js.executeScript("arguments[0].scrollIntoView();", run);
+			
+			run.click();			
+			Alert alert = driver.switchTo().alert();
+			alert.sendKeys("Hello World!");
+			Thread.sleep(5000);
+			alert.accept();
+			System.out.println(alert.getText());
+			Thread.sleep(5000);
+			alert.accept();
+			
+		} catch (Exception e) {
+			System.out.println("Exception: " + e.getMessage());
+		}
+	}
+}
---
+++ SeleniumDemo/src/alerts/ConfirmAlertAndDismiss.java
@@ -0,0 +1,38 @@
+package alerts;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class ConfirmAlertAndDismiss {
+
+	private static String driverPath;
+	private static String URL = "https://javascript.info/alert-prompt-confirm#prompt";
+	private static WebElement element;
+
+	public static void main(String[] args) {
+		try {
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\Public\\chromedriver.exe");
+			WebDriver driver = new ChromeDriver();
+			driver.get(URL);
+
+			WebElement run = driver.findElement(By.xpath("//div[7]//div[1]//div[1]//div[1]//a[1]"));
+
+			JavascriptExecutor js = (JavascriptExecutor) driver;
+			js.executeScript("arguments[0].scrollIntoView();", run);
+			
+			run.click();			
+			Alert alert = driver.switchTo().alert();
+			Thread.sleep(5000);
+			alert.dismiss();	
+			Thread.sleep(5000);
+			alert.accept();
+		} catch (Exception e) {
+			System.out.println("Exception: " + e.getMessage());
+		}
+	}
+}
