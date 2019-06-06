bb4c8762aa08b69c935c1370d1b6fa14b505da3a
ajayramola24 <ajayramola24@windows-server-2016-1.asia-south1-c.c.amplified-lamp-239212.internal>
2019-06-06 12:33:56 +0000

    ccc

--- SeleniumDemo/src/autoit/UploadFile.java
+++ SeleniumDemo/src/autoit/UploadFile.java
@@ -5,10 +5,20 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Ex {
+public class UploadFile {
 
 	public static String driverPath;
 	public static WebDriver driver;
+	
+	/* AutoIT script for uploading the file in Windows:
+	 *
+ 		ControlFocus("Open", "","Edit1")
+		Sleep(2000)
+		ControlSetText("Open", "", "Edit1", "C:\Users\Public\file.txt")
+		Sleep(2000)
+		ControlClick("Open", "", "Button1")
+		Sleep(2000)
+ 	 */
 
 	public static void main(String[] args) {
 		try {
@@ -19,7 +29,7 @@ public class Ex {
 			WebElement clickHere = driver.findElement(By.xpath("//button[@id='btnChooseFiles']"));
 			clickHere.click();
 			
-			Runtime.getRuntime().exec("C:\\Users\\Public\\ClickDocument.exe");
+			Runtime.getRuntime().exec("C:\\Users\\ajayramola24\\Documents\\ClickDocument.exe");
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
